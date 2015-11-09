28431cb3121730b2b3de1a32aaf5abb070256a01
Nish Tahir <nishtahir@outlook.com>
2015-11-09 17:00:30 +0200

    Added decent command line argument support

---
+++ src/main/java/com/nishtahir/ALangApplication.java
@@ -0,0 +1,10 @@
+package com.nishtahir;
+
+/**
+ * Created by Nish on 11/9/15.
+ */
+public class ALangApplication {
+    public static final String APP_NAME = "ALang";
+    public static final String VERSION = "0.0.1";
+    public static final String COPYRIGHT = "Copyright Nish Tahir 2015";
+}
--- src/main/java/com/nishtahir/Main.java
+++ src/main/java/com/nishtahir/Main.java
@@ -3,35 +3,98 @@ package com.nishtahir;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
-    /**
-     * Logger
-     */
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    Main(){
-        try {
-            ALangLexer lexer = new ALangLexer(new ANTLRFileStream("test.txt"));
-            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-            ALangParser parser = new ALangParser(tokenStream);
-            ParserRuleContext tree =  parser.compilationUnit();
-            ALangVisitor visitor = new ALangEvalVisitor();
-            visitor.visit(tree);
 
-            System.out.println("End parse ----");
+    private String[] args;
+    private Options options = new Options();
+    private Option help = new Option("h", "help", false, "print this message");
+    private Option version = new Option("version", false,
+            "more info about " + ALangApplication.APP_NAME);
+    private Option verbose = new Option("v", "verbose", false,
+            "show more information");
+
+    public Main(String[] args) {
+        this.args = args;
+        initOptions();
+    }
+
+    public static void main(String[] args) {
+        new Main(args).parse();
+    }
+
+
+    private void parse() {
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine cmd;
+
+        try {
+            cmd = commandLineParser.parse(options, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if (cmd.hasOption("h")) {
+            displayHelpInfo();
+        }
+
+        if (cmd.hasOption("version")) {
+            displayVersionInfo();
+        }
+
+        List<String> trailingArgs = cmd.getArgList();
+        List<String> files = new ArrayList<>(trailingArgs.size());
+        for (String path : trailingArgs) {
+            File file = FileUtils.getFile(path);
+            if (file.exists()) {
+                files.add(file.getAbsolutePath());
+            } else {
+                log.error(file.getAbsolutePath() + " is not a valid file path.");
+            }
+        }
+
+        try {
+            for (String path : files) {
+                ALangLexer lexer = new ALangLexer(new ANTLRFileStream(path));
+                CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+                ALangParser parser = new ALangParser(tokenStream);
+                ParserRuleContext tree = parser.compilationUnit();
+                ALangVisitor visitor = new ALangEvalVisitor();
+                visitor.visit(tree);
+            }
+
         } catch (IOException e) {
             log.error("An unexpected error occurred", e);
         }
     }
 
-    public static void main(String[] args) {
-        new Main();
+    public void displayHelpInfo() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("[file]", options);
+        System.exit(0);
     }
 
+    public void displayVersionInfo() {
+        System.out.println("Alang" + ALangApplication.VERSION + ", " + ALangApplication.COPYRIGHT);
+        System.exit(0);
+    }
+
+    public void initOptions() {
+        options.addOption(help);
+        options.addOption(verbose);
+        options.addOption(version);
+    }
 }
